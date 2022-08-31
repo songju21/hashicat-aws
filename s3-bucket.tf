825ba4bd2a119a92257bc6e9edc93abe18ba2af1
songju <songju021@gmail.com>
2022-08-31 08:25:16 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
-
 module "s3_bucket" {
   source  = "app.terraform.io/0831_oragnization/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = "${var.prefix}"
+  bucket = "my-s3-bucket-0831-songjutest"
   
 }
